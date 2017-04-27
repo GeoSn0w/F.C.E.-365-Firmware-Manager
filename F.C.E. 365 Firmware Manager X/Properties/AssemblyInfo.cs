1f2e5c5d022d702c9b121d948d40fbbc4df35697
GeoSn0w <GeoSn0w@users.noreply.github.com>
2017-04-27 12:05:26 -0700

    Add files via upload

---
+++ F.C.E. 365 Firmware Manager X/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("F.C.E. 365 Firmware Manager X")]
+[assembly: AssemblyDescription("F.C.E. 365 Firmware Manager X")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("F.C.E. 365 TV")]
+[assembly: AssemblyProduct("F.C.E. 365 Firmware Manager X")]
+[assembly: AssemblyCopyright("Copyright © GeoSn0w 2017")]
+[assembly: AssemblyTrademark("F.C.E. 365 Firmware Manager X")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("75522d4e-8730-469e-8f13-731122897b60")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("32.0.0.0")]
+[assembly: AssemblyFileVersion("32.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
